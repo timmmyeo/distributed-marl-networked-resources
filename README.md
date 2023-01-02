b84e8ee8170e2579934f6434e71bf284b8b383f5
Timothy Yeo <tgy19@ic.ac.uk>
2023-01-02 18:48:17 +0000

    [WIP] Redo the ene environment's constructor and observation space

---
+++ README.md
@@ -0,0 +1,4 @@
+In order to rebuild the custom environment:
+```
+pip install -e custom_environments
+```
--- custom_environments/envs/multi_data_center_env copy.py
+++ custom_environments/envs/multi_data_center_env copy.py
@@ -6,6 +6,41 @@ import numpy as np
 class MultiDataCenterEnvironment(gym.Env):
   metadata = {"render_modes": ["human"]}
 
+  def __init__(self, datacentres: list[dict[str, pd.DataFrame]], end_time: int):
+    # super(MultiDataCenterEnvironment, self).__init__()
+    
+    # Each element is a datacenter consisting of machines (indexed from 0 to n-1):
+    # [
+    #   {0: pd.DataFrame, 1: pd.DataFrame ...},
+    #   {0: pd.DataFrame, 1: pd.DataFrame ...}
+    # ]
+    self.datacentres = [{f"{i}": datacentre[machine_id] for (i, machine_id) in enumerate(datacentre)} for datacentre in datacentres]
+
+    self._workload = 0
+    self._workload_datacentre = 0
+
+    # Current time of the simulation
+    self.current_time = 0
+    # End time of the simulation
+    self.end_time = end_time
+
+    # [cpu1, cpu2, ..., cpu10, workload_requirement, data_center]
+    self.observation_space = spaces.Dict(
+      {
+        **{f"{datacentre_number}": spaces.Box(low=0, high=100, shape=(len(datacentre),), dtype=np.float64) for datacentre_number, datacentre in enumerate(self.datacentres)},
+        "workload": spaces.Box(low=0, high=100, shape=(1,), dtype=np.float64),
+        "workload_datacentre": spaces.Discrete(2),
+      }
+    )
+    
+    # One action per machine and a no-op; choose to send the workload to a machine, or choose not to do anything
+    self.action_space = spaces.Discrete(11)
+  
+  # def _get_obs(self):
+  #   return {
+      
+  #   }
+    
   def _generate_workload(self):
     cpu_requirement = np.random.rand() * 100
     return cpu_requirement
@@ -16,51 +51,25 @@ class MultiDataCenterEnvironment(gym.Env):
     if r <= epsilon:
       data_centre_to_send_work = 1 - data_centre_to_send_work
     return data_centre_to_send_work
-
-  def __init__(self, machines_data: list[list[np.float32]], datacentre_mapping: dict[int, int], num_datacentres: int):
-    # super(MultiDataCenterEnvironment, self).__init__()
     
-    assert len(machines_data) > 0
-    assert machines_data[0] > 0
-    assert num_datacentres <= len(machines_data)
 
-    self.num_datacentres = num_datacentres
-    self.machines_data = machines_data
-
-    # Current time of the simulation
-    self.current_time = 0
-    # Current state of the simulation
-    self._workload = self._generate_workload()
-    self._workload_datacentre = self._get_workload_datacentre()
-    self._machines_curr_state: list[np.float32] = [machine_data[0] for machine_data in self.machines_data]
-
-    # (cpu1, cpu2, ..., cpu10, workload_requirement, data_center)
-    self.observation_space = spaces.Dict(
-      {
-        "machines_curr_state": spaces.Tuple((spaces.Box(low=0, high=100, dtype=np.float32) for _ in range(len(machines_data)))),
-        "workload": spaces.Box(low=0, high=100, dtype=np.float32),
-        "workload_datacentre": spaces.Discrete(num_datacentres),
-      }
-    )
-    
-    # One action per machine and a no-op; choose to send the workload to a machine, or choose not to do anything
-    self.action_space = spaces.Discrete(len(machines_data) + 1)
   
-  # def _get_obs(self):
-  #   return {
-      
-  #   }
-
   def reset(self, seed=None, options=None):
     # We need the following line to seed self.np_random
     super().reset(seed=seed)
 
-    # Current time of the simulation
     self.current_time = 0
-    # Current state of the simulation
+    self._datacentre_to_send_work = 0
+
+    # Each machine in a datacenter's first row in the pandas dataframe
+    # [
+    #   {1: 80.4, 2: 50.3 ...},
+    #   {1: 30.5, 2: 40.1 ...}
+    # ]
+    self._datacentres_curr_state = [{f"{i}": datacentre[machine_id].loc[self.current_time, 'cpu_util_percent'] for (i, machine_id) in enumerate(datacentre)} for datacentre in self.datacentres]
+
     self._workload = self._generate_workload()
     self._workload_datacentre = self._get_workload_datacentre()
-    self._machines_curr_state: list[np.float32] = [machine_data[0] for machine_data in self.machines_data]
 
     observation = {
       **{f"{datacentre_number}": datacentre for datacentre_number, datacentre in enumerate(self._datacentres_curr_state)},
